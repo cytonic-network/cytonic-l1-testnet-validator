ce252ccd563e77e3802597fcf9b7b2ce56ca3ee5
ChronosXYZ <chronosxyz000@gmail.com>
2024-12-20 21:21:23 +0300

    Update seed node

--- scripts/init-node.sh
+++ scripts/init-node.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-SEED_NODES="fc5163b624a95a9c3786248516ad4bd7cd1cfd5d@52.201.52.98:26000"
+SEED_NODES="7f4b8fdde3b6dc62f40cab3b18fd36b5e376729d@52.201.52.98:26656"
 
 ./scripts/run-node-command.sh node init $1
 cp genesis.json data/config/
 
 sed -i "s/seeds = \"\"/seeds = \"$SEED_NODES\"/" data/config/config.toml
+sed -i "s/persistent_peers = \"\"/persistent_peers = \"$SEED_NODES\"/" data/config/config.toml
